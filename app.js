a84d5389dcdaffa224a553b7a57b492997102351
andytompkins <andy.tompkins@clearcapital.com>
2014-05-08 21:47:31 -0700

    Fixed bug

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 angular.module('template-repeat-demo', ['template-repeat'])
 .controller('DemoController', [ '$scope', function DemoController($scope) {
 	
-	$scope.items = [];
+	var itemList = [];
 	
 	var numObjs = 5;
 	for (var i = 0; i < numObjs; i++) {
 		var obj = {
 			id: i
 		};
-		$scope.items.push(obj);
+		itemList.push(obj);
 	}
-	console.log(JSON.stringify($scope.items));	
+	$scope.items = itemList;
 }]);
