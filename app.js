956fffd0ba6e0c9b085655d812c26fec050693c5
andytompkins <andy.tompkins@clearcapital.com>
2014-05-08 22:01:29 -0700

    Tinkering with numRows

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 angular.module('template-repeat-demo', ['template-repeat'])
 .controller('DemoController', [ '$scope', function DemoController($scope) {
 	
+	$scope.numRows = 10;
+	
 	var itemList = [];
 	
-	var numObjs = 5;
+	var numObjs = 2000;
 	for (var i = 0; i < numObjs; i++) {
 		var obj = {
 			id: i
 		};
 		itemList.push(obj);
 	}
-	$scope.items = itemList;
+	$scope.allItems = itemList;
+	$scope.items = itemList.slice(0, $scope.numRows);
 }]);
