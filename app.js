c9c1b111a67867344a333e3e483268da2e1862e4
andytompkins <andy.tompkins@clearcapital.com>
2014-05-08 21:40:27 -0700

    Fixed up the fake items array

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 angular.module('template-repeat-demo', [])
 .controller('DemoController', [ '$scope', function DemoController($scope) {
-	$scope.items = [
-		{}, {}, {}, {}
-	];
+	
+	$scope.items = [];
+	
+	var numObjs = 5;
+	for (var i = 0; i < numObjs; i++) {
+		var obj = {
+			id: i
+		};
+		$scope.items.push(obj);
+	}
+	
 }]);
